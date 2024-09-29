9dddb7e0d11c21d0908aba7c9a609a812dc5bda7
Matthieu Beauchamp-Boulay <matthieu.beauchamp-boulay@polymtl.ca>
2024-09-29 01:10:13 -0400

    -m

--- .config/nvim/lua/kickstart/plugins/cmp.lua
+++ .config/nvim/lua/kickstart/plugins/cmp.lua
@@ -1,3 +1,11 @@
+local truncate = function(text, max_width)
+  if #text > max_width then
+    return string.sub(text, 1, max_width) .. '…'
+  else
+    return text
+  end
+end
+
 return {
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
@@ -34,6 +42,7 @@ return {
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'onsails/lspkind.nvim',
     },
     config = function()
       -- See `:help cmp`
@@ -49,6 +58,79 @@ return {
         },
         completion = { completeopt = 'menu,menuone,noinsert' },
 
+        -- https://github.com/wookayin/dotfiles/blob/c6a7c1c38518f8448b5ec8e6bd288eb4206449e4/nvim/lua/config/lsp.lua#L422
+        -- Added this to get completion sources
+        formatting = {
+          format = function(entry, vim_item)
+            -- Truncate the item if it is too long
+            vim_item.abbr = truncate(vim_item.abbr, 80)
+
+            -- fancy icons and a name of kind
+            pcall(function() -- protect the call against potential API breakage (lspkind GH-45).
+              local lspkind = require 'lspkind'
+              vim_item.kind_symbol = (lspkind.symbolic or lspkind.get_symbol)(vim_item.kind)
+              vim_item.kind = ' ' .. vim_item.kind_symbol .. ' ' .. vim_item.kind
+            end)
+
+            -- The 'menu' section: source, detail information (lsp, snippet), etc.
+            -- set a name for each source (see the sources section below)
+            vim_item.menu = ({
+              buffer = 'Buffer',
+              nvim_lsp = 'LSP',
+              ultisnips = '',
+              nvim_lua = 'Lua',
+              latex_symbols = 'Latex',
+            })[entry.source.name] or string.format('%s', entry.source.name)
+
+            -- highlight groups for item.menu
+            vim_item.menu_hl_group = ({
+              buffer = 'CmpItemMenuBuffer',
+              nvim_lsp = 'CmpItemMenuLSP',
+              path = 'CmpItemMenuPath',
+              ultisnips = 'CmpItemMenuSnippet',
+            })[entry.source.name] -- default is CmpItemMenu
+
+            -- detail information (optional)
+            local cmp_item = entry:get_completion_item() --- @type lsp.CompletionItem
+
+            if entry.source.name == 'nvim_lsp' then
+              -- Display which LSP servers this item came from.
+              local lspserver_name = nil
+              pcall(function()
+                lspserver_name = entry.source.source.client.name
+                vim_item.menu = vim_item.menu .. ' [' .. lspserver_name .. ']'
+              end)
+
+              -- Some language servers provide details, e.g. type information.
+              -- The details info hide the name of lsp server, but mostly we'll have one LSP
+              -- per filetype, and we use special highlights so it's OK to hide it..
+              local detail_txt = (function(cmp_item)
+                if not cmp_item.detail then
+                  return nil
+                end
+                --   if lspserver_name == 'pyright' and cmp_item.detail == 'Auto-import' then
+                --     local label = (cmp_item.labelDetails or {}).description
+                --     return label and (' ' .. truncate(label, 20)) or nil
+                --   else
+                return truncate(cmp_item.detail, 50)
+                --   end
+              end)(cmp_item)
+
+              if detail_txt then
+                vim_item.menu = vim_item.menu .. ' (' .. detail_txt .. ')'
+                vim_item.menu_hl_group = 'CmpItemMenuDetail'
+              end
+              -- elseif entry.source.name == 'ultisnips' then
+              --   if (cmp_item.snippet or {}).description then
+              --     vim_item.menu = truncate(cmp_item.snippet.description, 40)
+              --   end
+            end
+
+            -- Add a little bit more padding
+            vim_item.menu = ' ' .. vim_item.menu
+            return vim_item
+          end,
+        },
         -- For an understanding of why these mappings were
         -- chosen, you will need to read `:help ins-completion`
         --
